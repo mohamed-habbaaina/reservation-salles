f39f3f46a3e86a3f346e768988c31ffb1646e5a3
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-12-12 19:23:22 +0100

    préparation de projet

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+
+</footer>
+<script>
+    humburger = document.querySelector(".hamburger");
+    hamburger.onclick = function() {
+        navBar = document.querySelector(".nav-bar");
+        navBar.classList.toggle("active");
+    }
+</script>
